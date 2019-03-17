4889d7774b855b64e0e8bfba432a711ceb1433f5
Nachiket <nachiket.kelkar@colorado.edu>
2019-03-17 09:03:21 -0600

    Error checking in gpio and created temperature sensor i2c files

--- gpio.c
+++ gpio.c
@@ -1,31 +1,54 @@
+/************************************************************************************************
+* File name   : gpio.c                                                                          *
+* Authors     : Nachiket Kelkar and Puneet Bansal                                               *
+* Description : The functions used for gpio operations. Setting the direction of pin and        *
+* 				the value. This functions are restricted for use of only USER LED pins.         *
+* Tools used  : GNU make.                                                                       *
+************************************************************************************************/
+
+/* Including standard libraries */
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Including user libraries */
 #include "gpio.h"
+
+/* gpio_pin high and low value. If different valid range is required then define different 
+*  values with the same define names */
+#define gpio_low 53
+#define gpio_high 56
 
 
 void gpio_init(int gpio_pin,int gpio_direction)
 {
 	FILE *fp;
 	char *file = (char*)malloc(40);
-	fp = fopen("/sys/class/gpio/export","w");
-	fprintf(fp,"%d",gpio_pin);
-	fclose(fp);
-	sprintf(file,"/sys/class/gpio/gpio%d/direction",gpio_pin);
-	fp = fopen(file,"w");
-	if(gpio_direction == out)
+	if(gpio_pin >= gpio_low && gpio_pin <= gpio_high)
 	{
-		fprintf(fp,"out");
-	}
-	else if(gpio_direction == in)
-	{
-		fprintf(fp,"in");
+		fp = fopen("/sys/class/gpio/export","w");
+		fprintf(fp,"%d",gpio_pin);
+		fclose(fp);
+		sprintf(file,"/sys/class/gpio/gpio%d/direction",gpio_pin);
+		fp = fopen(file,"w");
+		if(gpio_direction == out)
+		{
+			fprintf(fp,"out");
+		}
+		else if(gpio_direction == in)
+		{
+			fprintf(fp,"in");
+		}
+		else
+		{
+			printf("Enter direction only as in or out");
+		}
+		fclose(fp);
 	}
 	else
 	{
-		printf("Enter direction only as in or out");
+		printf("Enter the pin numbers in range %d - %d",gpio_low,gpio_high);
 	}
-	fclose(fp);
-	free(file);
+		free(file);
 }
 
 
@@ -33,21 +56,28 @@ void gpio_write_value(int gpio_pin, int gpio_value)
 {
 	FILE *fp;
 	char *file = (char*)malloc(40);
-	sprintf(file,"/sys/class/gpio/gpio%d/value",gpio_pin);
-	fp = fopen(file,"w");
-	if(gpio_value == low)
+	if(gpio_pin >= gpio_low && gpio_pin <= gpio_high)
 	{
-		fprintf(fp,"%d",low);
-	}
-	else if(gpio_value == high)
-	{
-		fprintf(fp,"%d",high);
+		sprintf(file,"/sys/class/gpio/gpio%d/value",gpio_pin);
+		fp = fopen(file,"w");
+		if(gpio_value == low)
+		{
+			fprintf(fp,"%d",low);
+		}
+		else if(gpio_value == high)
+		{
+			fprintf(fp,"%d",high);
+		}
+		else
+		{
+			printf("Enter value only as low or high");
+		}
+		fclose(fp);
 	}
 	else
 	{
-		printf("Enter value only as low or high");
+		printf("Enter the pin numbers in range %d - %d",gpio_low,gpio_high);
 	}
-	fclose(fp);
 	free(file);
 }
 
--- gpio.h
+++ gpio.h
@@ -1,4 +1,12 @@
+/************************************************************************************************
+* File name   : gpio.h                                                                          *
+* Authors     : Nachiket Kelkar and Puneet Bansal                                               *
+* Description : The functions used for gpio operations. Setting the direction of pin and        *
+* 				the value.                                                                      *
+* Tools used  : GNU make.                                                                       *
+************************************************************************************************/
 
+/**************** Enumerations used for gpio direction and gpio value ****************/
 enum gpio_direction{
 in = 0,
 out,
@@ -9,7 +17,8 @@ low = 0,
 high,
 };
 
-/*
+/********************** Functions for the gpio operations *********************/
+/* 
 * Function name:- gpio_init
 * Description:- The functon takes the gpio pin number and assignes it as input pin or output pin.
 * @param:- int (gpio pin number), int (gpio pin direction)
