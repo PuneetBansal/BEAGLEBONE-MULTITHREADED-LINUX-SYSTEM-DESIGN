749463bf125df9ce04b74d5f1d0e5c1c55b89b39
PuneetBansal <puneet.bansal@colorado.edu>
2019-03-18 18:57:25 -0600

    Adding myi2c

---
+++ myi2c.c
@@ -0,0 +1,59 @@
+ref:https://elinux.org/Interfacing_with_I2C_Devices
+
+#include<linux/i2c.h>
+#include<linux/i2c-dev.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "myi2c.h";
+
+int myi2cInit(uint8_t slaveAdd)
+{
+char fileName[50];
+int fileDescrip;
+sprintf(fileName,"/dev/i2c-2");
+fileDescrip=open(fileName,O_RDWR);
+
+if(fileDescrip < 0)
+{
+  perror("Failed to open i2c file");
+  exit(1);
+}
+
+if(ioctl(fileDescrip, I2C_SLAVE, slaveAdd) < 0) 
+{
+  perror("Failed to communicate with slave"); //or use errno
+  exit(1);
+}
+return fileDescrip;
+}
+
+uint8_t* myi2cRead(int fileDescrip,uint8_t len)
+{
+uint8_t readBuffer[2];
+if (read(fileDescrip,buf,len) != len)
+{
+  perror("Read failed");
+  return NULL;
+  //check whether we need to return or not.
+}
+return readBuffer;
+}
+
+int myi2cWrite(int fileDescrip, uint8_t writeBuffer[], uint8_t len)
+{
+
+if (write(fileDescrip,writeBuffer,len) != len)
+{
+perror("Write failed");
+return -1;
+}
+return 0;
+
+}
---
+++ myi2c.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+#define slaveAddFloat 0x39
+#define slaveAddGnd 0x29
+#define slaveAddVdd 0x49
+
+/*Light Sensor Register addressess*/
+#define CMDBYTE 0x80
+#define CMDWORD 0xA0
+#define CNTRLREG 0x00
+#define INTCTL 0x06
+#define DATA0LOW 0x0c
+#define DATA0HIGH 0x0d
+#define DATA1LOW 0x0e
+#define DATA1HIGH 0x0f
+
+//char fileName[50];
+int myi2cInit(uint8_t slaveAdd);
+uint8_t* myi2cRead(int, uint8_t);
+int myi2cWrite(int,uint8_t,uint8_t);
+
+
